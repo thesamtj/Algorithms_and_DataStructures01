77bf45845006675bcde9e8ed145c8ca5d5c890f5
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-01-31 11:38:06 +0100

    Add more methods

--- JavaScript/SinglyLinkedList/SinglyLinkedList.js
+++ JavaScript/SinglyLinkedList/SinglyLinkedList.js
@@ -1,32 +1,143 @@
 class Node {
-     constructor(value){
-         this.value = value
-         this.next = null
-     }
- }
- 
- class LinkedList {
-     constructor(value) {
-         const newNode = new Node(value)
-         this.head = newNode
-         this.tail = this.head
-         this.length = 1
-     }
- 
-     push(value) {
-         const newNode = new Node(value)
-         if (!this.head) {
-             this.head = newNode
-             this.tail = newNode
-         } else {
-             this.tail.next = newNode
-             this.tail = newNode
-         }
-         this.length++
-         return this
-     }
- }
- 
- let myLinkedList = new LinkedList(7)
- myLinkedList.push(4)
- 
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        this.length++;
+        return this;
+    }
+
+  pop() {
+   if (!this.head) return undefined;
+
+   let pre = this.head;
+   let temp = this.head;
+
+   while(temp.next) {
+     pre = temp;
+     temp = temp.next;
+   }
+
+   this.tail = pre;
+   this.tail.next = null;
+   this.length--;
+
+   if (this.length === 0) {
+     this.head = null;
+     this.tail = null;
+   }
+
+   return temp;
+   
+  }
+
+   unshift(value) {
+      const newNode = new Node(value);
+
+      if (!this.head) {
+         this.head = newNode;
+         this.tail = newNode;
+      } else {
+         newNode.next = this.head;
+         this.head = newNode;
+      }      
+      
+      this.length++;
+      return this;
+   }
+
+   shift() {
+      if (!this.head) return undefined;
+
+      let temp = this.head;
+      this.head = this.head.next;      
+      
+      this.length--;
+      if (this.length === 0) {
+         this.tail = null;
+      }
+      
+      temp.next = null;
+      return temp;
+   }
+
+   get(index) {
+      if (index < 0 || index >= this.length) return undefined;
+      
+      let temp = this.head;      
+      for (let i = 0; i < index; i++) {
+         temp = temp.next;
+      }
+
+      return temp;      
+      
+   }
+
+   set(index, value) {
+      let temp = this.get(index);
+
+      if (temp) {
+         temp.value = value;
+         return true;
+      }
+      
+      return false;
+   }
+
+   insert(index, value) {
+      if (index < 0 || index > this.length) return false;
+      if (index === 0) return this.unshift(value);
+      if (index === this.length) return this.push(value);
+      
+      const newNode = new Node(value);   
+      const temp = this.get(index - 1);      
+      newNode.next = temp.next;
+      temp.next = newNode;
+      this.length++;   
+      
+      return true;
+   }
+
+   remove(index) {
+      if (index < 0 || index >= this.length) return undefined;
+      if (index === 0) return this.shift();
+      if (index === this.length - 1) return this.pop();
+
+      const prev = this.get(index - 1);
+      const temp = prev.next;
+      
+      prev.next = temp.next;
+      temp.next = null;
+      this.length--;
+
+      return temp;      
+   }
+  
+}
+
+let myLinkedList = new LinkedList(2)
+myLinkedList.push(1)
+myLinkedList.push(3)
+myLinkedList.push(5)
+myLinkedList.push(7)
