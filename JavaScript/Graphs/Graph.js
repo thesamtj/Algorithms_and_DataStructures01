8a6824654cbaff5a13e52e8cc0e465dd653e3630
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-04-18 11:32:56 +0100

    Implement remove edge update

--- JavaScript/Graphs/Graph.js
+++ JavaScript/Graphs/Graph.js
@@ -15,24 +15,32 @@ class Graph {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
       this.adjacencyList[vertex1].push(vertex2)
       this.adjacencyList[vertex2].push(vertex1)
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
   removeEdge(vertex1, vertex2) {
     if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
-      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
-      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
-      return true;
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        (v) => v !== vertex2,
+      )
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1,
+      )
+      return true
     }
-    return false;
+    return false
   }
-
 }
 
 let myGraph = new Graph()
 
-myGraph.addVertex('1')
-myGraph.addVertex('2')
+myGraph.addVertex('A')
+myGraph.addVertex('B')
+myGraph.addVertex('C')
+myGraph.addEdge('A', 'B')
+myGraph.addEdge('B', 'C')
+myGraph.addEdge('C', 'A')
+
 myGraph
