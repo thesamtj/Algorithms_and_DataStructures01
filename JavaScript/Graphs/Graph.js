45c5bcc6f5ed35231f33bcf57cdc4113688967cc
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-04-18 11:19:55 +0100

    Implement add Edge

--- JavaScript/Graphs/Graph.js
+++ JavaScript/Graphs/Graph.js
@@ -1,16 +1,28 @@
 class Graph {
-    constructor(){
-        this.adjacencyList = {}
-    }
+  constructor() {
+    this.adjacencyList = {}
+  }
 
-    addVertex(vertex) {
-        if(!this.adjacencyList[vertex]) {
-            this.adjacencyList[vertex] = [];
-            return true
-        }
-        return false
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = []
+      return true
     }
+    return false
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1].push(vertex2)
+      this.adjacencyList[vertex2].push(vertex1)
+      return true;
+    }
+    return false;
+  }
 }
 
-let myGraph = new Graph();
+let myGraph = new Graph()
+
+myGraph.addVertex('1')
+myGraph.addVertex('2')
 myGraph
