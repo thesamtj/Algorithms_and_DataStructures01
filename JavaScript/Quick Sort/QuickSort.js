35bf1445ccf80d09c320db207351c2f5d738101d
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-05-02 15:15:37 +0100

    Implement Quick sort

--- JavaScript/Quick Sort/QuickSort.js
+++ JavaScript/Quick Sort/QuickSort.js
@@ -6,7 +6,7 @@ function swap(array, firstIndex, secondIndex) {
 
 function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
   let swapIndex = pivotIndex
-  for (let i = pivotIndex; i < endIndex; i++) {
+  for (let i = pivotIndex + 1; i <= endIndex; i++) {
     if (array[i] < array[pivotIndex]) {
       swapIndex++
       swap(array, swapIndex, i)
@@ -15,3 +15,15 @@ function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
   swap(array, pivotIndex, swapIndex)
   return swapIndex
 }
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right)
+    quickSort(array, left, pivotIndex - 1)
+    quickSort(array, pivotIndex + 1, right)
+  }
+
+  return array
+}
+
+quickSort([4, 6, 1, 7, 3, 2, 5])
