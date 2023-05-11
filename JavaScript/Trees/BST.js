75ccfab618ebac00b07b1169f9603cee194e93e3
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-05-11 10:09:36 +0100

    Implement DFS Post Order

--- JavaScript/Trees/BST.js
+++ JavaScript/Trees/BST.js
@@ -1,93 +1,106 @@
 class Node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 class BST {
   constructor() {
-    this.root = null
+    this.root = null;
   }
 
   insert(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
     if (this.root === null) {
-      this.root = newNode
-      return this
+      this.root = newNode;
+      return this;
     }
 
-    let temp = this.root
+    let temp = this.root;
     while (true) {
-      if (newNode.value === temp.value) return undefined
+      if (newNode.value === temp.value) return undefined;
       if (newNode.value < temp.value) {
         if (!temp.left) {
-          temp.left = newNode
-          return this
+          temp.left = newNode;
+          return this;
         }
-        temp = temp.left
+        temp = temp.left;
       } else {
         if (!temp.right) {
-          temp.right = newNode
-          return this
+          temp.right = newNode;
+          return this;
         }
-        temp = temp.right
+        temp = temp.right;
       }
     }
   }
 
   contains(value) {
-    if (!this.root) return false
+    if (!this.root) return false;
 
-    let temp = this.root
+    let temp = this.root;
     while (temp) {
       if (value < temp.value) {
-        temp = temp.left
+        temp = temp.left;
       } else if (value > temp.value) {
-        temp = temp.right
+        temp = temp.right;
       } else {
-        return true
+        return true;
       }
     }
-    return false
+    return false;
   }
 
   BFS() {
-    let currentNode = this.root
-    let results = []
-    let queue = []
-    queue.push(currentNode)
+    let currentNode = this.root;
+    let results = [];
+    let queue = [];
+    queue.push(currentNode);
 
     while (queue.length) {
-      currentNode = queue.shift()
-      results.push(currentNode.value)
-      if (currentNode.left) queue.push(currentNode.left)
-      if (currentNode.right) queue.push(currentNode.right)
+      currentNode = queue.shift();
+      results.push(currentNode.value);
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
     }
 
-    return results
+    return results;
   }
 
   DFSPreOrder() {
-    let results = []
+    let results = [];
 
     function traverse(currentNode) {
-      results.push(currentNode.value)
-      if (currentNode.left) traverse(currentNode.left)
-      if (currentNode.right) traverse(currentNode.right)
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
     }
 
-    traverse(this.root)
-    return results
+    traverse(this.root);
+    return results;
+  }
+
+  DFSPostOrder() {
+    let results = [];
+
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+      results.push(currentNode.value);
+    }
+
+    traverse(this.root);
+    return results;
   }
 }
 
-let myTree = new BST()
-myTree.insert(47)
-myTree.insert(21)
-myTree.insert(76)
-myTree.insert(18)
-myTree.insert(27)
-myTree.insert(52)
-myTree.insert(82)
+let myTree = new BST();
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
