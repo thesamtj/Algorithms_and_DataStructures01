ac81f209204cfa86b6c6455656e4983d1e4bb936
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-05-11 10:19:52 +0100

    Implement DFS In Order

--- JavaScript/Trees/BST.js
+++ JavaScript/Trees/BST.js
@@ -94,6 +94,19 @@ class BST {
     traverse(this.root);
     return results;
   }
+
+  DFSInOrder() {
+    let results = [];
+
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
