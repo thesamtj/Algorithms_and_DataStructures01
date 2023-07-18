d9ff77fe7dfd96310c466b34588aeec89ced8301
Samuel Tijani <54986149+thesamtj@users.noreply.github.com>
2023-07-18 15:56:06 +0100

    Implement String Reversal

--- Algo/JavaScript/reversestring/index.js
+++ Algo/JavaScript/reversestring/index.js
@@ -6,6 +6,41 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+//--- Solutions
+
+//--- Using classical For Loop
+// function reverse(str) {
+//     let reversedStr = "";
+//     let strLength = str.length;
+
+//     for (let index = strLength-1; index >= 0; index--) {
+//         const element = str[index];
+//         reversedStr += element;
+//         debugger;
+//     }
+
+//     return reversedStr;
+// }
+
+//--- Using  For...of Loop
+// function reverse(str) {
+//     let reversedStr = "";
+
+//     for (let character of str) {
+//         reversedStr = character + reversedStr;
+//     }
+
+//     return reversedStr;
+// }
+
+//--- Using  Array reduce method
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// }
+
+//--- Using Arrary reverse method
+// function reverse(str) {
+//     return str.split('').reverse().join('');
+// }
 
 module.exports = reverse;
